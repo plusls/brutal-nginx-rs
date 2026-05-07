085f654176fc425f08341c407a44020937e9078b
plusls <plusls@qq.com>
2026-05-07 23:08:44 +0800

    feat: 实现 http 支持

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,58 @@
-use core::{
-    ffi::{c_char, c_void},
-    ptr,
-};
 use std::{alloc::Layout, ptr::NonNull, str::FromStr};
 
 use nginx_sys::{
-    __socket_type_SOCK_STREAM, AF_UNIX, IPPROTO_TCP, NGX_LOG_ERR, NGX_OK, TCP_CONGESTION,
-    ngx_stream_compile_complex_value, ngx_stream_compile_complex_value_t,
-    ngx_stream_complex_value_t, setsockopt,
+    __socket_type_SOCK_STREAM, AF_UNIX, IPPROTO_TCP, NGX_LOG_ERR, TCP_CONGESTION, setsockopt,
 };
 use ngx::{
     allocator::Allocator,
-    core::{NGX_CONF_ERROR, NGX_CONF_OK, Pool, Status},
-    ffi::{
-        NGX_CONF_TAKE1, NGX_LOG_EMERG, NGX_STREAM_MODULE, NGX_STREAM_SRV_CONF,
-        NGX_STREAM_SRV_CONF_OFFSET, ngx_command_t, ngx_conf_t, ngx_int_t, ngx_module_t, ngx_str_t,
-        ngx_stream_module_t, ngx_uint_t,
-    },
+    core::{Pool, Status},
+    ffi::{NGX_LOG_EMERG, ngx_command_t, ngx_conf_t, ngx_str_t},
     http::{Merge, MergeConfigError},
-    log::DebugMask,
-    ngx_conf_log_error, ngx_log_debug, ngx_log_error, ngx_string,
+    ngx_conf_log_error, ngx_log_debug, ngx_log_error,
 };
 
-use crate::{
-    stream::{Session, StreamModule, StreamModuleServerConf, StreamPhase, StreamSessionHandler},
-    value::{NginxBool, NginxStreamComplexValue, NginxStreamValue},
+use crate::value::{
+    ComplexValueTrait, NginxBool, NginxComplexValue, NginxHandlerCtxTrait, NginxValue,
 };
 
-mod value;
+pub mod value;
 
 #[cfg(ngx_feature = "stream")]
 pub mod stream;
-struct Module;
 
-impl stream::StreamModule for Module {
-    fn module() -> &'static ngx_module_t {
-        unsafe { &*::core::ptr::addr_of!(ngx_stream_brutal_module) }
-    }
+pub struct Module;
 
-    unsafe extern "C" fn postconfiguration(cf: *mut ngx_conf_t) -> ngx_int_t {
-        // SAFETY: this function is called with non-NULL cf always
-        let cf = unsafe { &mut *cf };
-        stream::add_phase_handler::<BrutalSessionHandler>(cf)
-            .map_or(Status::NGX_ERROR, |_| Status::NGX_OK)
-            .into()
+#[derive(Debug)]
+pub struct ModuleConfig<CVT> {
+    pub enable: Option<NginxValue<NginxBool, CVT>>,
+    pub rate: Option<NginxValue<u64, CVT>>,
+    pub cwnd_gain: Option<NginxValue<u32, CVT>>,
+}
+
+impl<CVT> Default for ModuleConfig<CVT> {
+    fn default() -> Self {
+        Self {
+            enable: Default::default(),
+            rate: Default::default(),
+            cwnd_gain: Default::default(),
+        }
     }
 }
 
-#[derive(Debug, Default)]
-struct ModuleConfig {
-    enable: Option<NginxStreamValue<NginxBool>>,
-    rate: Option<NginxStreamValue<u64>>,
-    cwnd_gain: Option<NginxStreamValue<u32>>,
-}
-
-impl ModuleConfig {
-    fn get_brutal_param(&mut self, session: &mut Session) -> Option<BrutalParams> {
+impl<CVT> ModuleConfig<CVT> {
+    pub fn get_brutal_param<CTX: NginxHandlerCtxTrait<NginxComplexValueType = CVT>>(
+        &mut self,
+        ctx: &mut CTX,
+    ) -> Option<BrutalParams> {
         if let Some(enable) = &mut self.enable {
-            match enable.extract(Some(session)) {
+            match enable.extract(Some(ctx)) {
                 Ok(enable) => {
                     if !enable.as_bool() {
                         return None;
                     }
                 }
                 Err(err) => {
-                    ngx_log_error!(NGX_LOG_EMERG, session.log(), "{err}",);
+                    ngx_log_error!(NGX_LOG_EMERG, ctx.log(), "{err}",);
                 }
             }
         } else {
@@ -72,21 +60,21 @@ impl ModuleConfig {
         }
 
         if let Some(rate) = &mut self.rate {
-            let mut cwnd_gain_default = NginxStreamValue::Static(15);
+            let mut cwnd_gain_default = NginxValue::Static(15);
             let cwnd_gain = self.cwnd_gain.as_mut().unwrap_or(&mut cwnd_gain_default);
 
-            let rate = match rate.extract(Some(session)) {
+            let rate = match rate.extract(Some(ctx)) {
                 Ok(rate) => rate,
                 Err(err) => {
-                    ngx_log_error!(NGX_LOG_EMERG, session.log(), "{err}",);
+                    ngx_log_error!(NGX_LOG_EMERG, ctx.log(), "{err}",);
                     return None;
                 }
             };
 
-            let cwnd_gain = match cwnd_gain.extract(Some(session)) {
+            let cwnd_gain = match cwnd_gain.extract(Some(ctx)) {
                 Ok(cwnd_gain) => cwnd_gain,
                 Err(err) => {
-                    ngx_log_error!(NGX_LOG_EMERG, session.log(), "{err}",);
+                    ngx_log_error!(NGX_LOG_EMERG, ctx.log(), "{err}",);
                     return None;
                 }
             };
@@ -100,69 +88,13 @@ impl ModuleConfig {
 
 #[repr(C, packed)]
 // TCP_BRUTAL_PARAMS expects the packed 12-byte kernel ABI: u64 rate + u32 cwnd_gain.
-struct BrutalParams {
+pub struct BrutalParams {
     rate: u64,
     cwnd_gain: u32,
 }
 
-unsafe impl StreamModuleServerConf for Module {
-    type ServerConf = ModuleConfig;
-}
-
-static mut NGX_STREAM_BRUTAL_COMMANDS: [ngx_command_t; 4] = [
-    ngx_command_t {
-        name: ngx_string!("brutal"),
-        type_: (NGX_STREAM_SRV_CONF | NGX_CONF_TAKE1) as ngx_uint_t,
-        set: Some(ngx_stream_brutal_set),
-        conf: NGX_STREAM_SRV_CONF_OFFSET,
-        offset: 0,
-        post: ptr::null_mut(),
-    },
-    ngx_command_t {
-        name: ngx_string!("brutal_rate"),
-        type_: (NGX_STREAM_SRV_CONF | NGX_CONF_TAKE1) as ngx_uint_t,
-        set: Some(ngx_stream_brutal_rate_commands_set),
-        conf: NGX_STREAM_SRV_CONF_OFFSET,
-        offset: 0,
-        post: ptr::null_mut(),
-    },
-    ngx_command_t {
-        name: ngx_string!("brutal_cwnd_gain"),
-        type_: (NGX_STREAM_SRV_CONF | NGX_CONF_TAKE1) as ngx_uint_t,
-        set: Some(ngx_stream_brutal_cwnd_gain_commands_set),
-        conf: NGX_STREAM_SRV_CONF_OFFSET,
-        offset: 0,
-        post: ptr::null_mut(),
-    },
-    ngx_command_t::empty(),
-];
-
-static NGX_STREAM_BRUTAL_MODULE_CTX: ngx_stream_module_t = ngx_stream_module_t {
-    preconfiguration: Some(Module::preconfiguration),
-    postconfiguration: Some(Module::postconfiguration),
-    create_main_conf: None,
-    init_main_conf: None,
-    create_srv_conf: Some(Module::create_srv_conf),
-    merge_srv_conf: Some(Module::merge_srv_conf),
-};
-
-// Generate the `ngx_modules` table with exported modules.
-// This feature is required to build a 'cdylib' dynamic module outside of the NGINX buildsystem.
-#[cfg(feature = "export-modules")]
-ngx::ngx_modules!(ngx_stream_brutal_module);
-
-#[used]
-#[allow(non_upper_case_globals)]
-#[cfg_attr(not(feature = "export-modules"), unsafe(no_mangle))]
-pub static mut ngx_stream_brutal_module: ngx_module_t = ngx_module_t {
-    ctx: &raw const NGX_STREAM_BRUTAL_MODULE_CTX as _,
-    commands: unsafe { &raw mut NGX_STREAM_BRUTAL_COMMANDS[0] },
-    type_: NGX_STREAM_MODULE as _,
-    ..ngx_module_t::default()
-};
-
-impl Merge for ModuleConfig {
-    fn merge(&mut self, prev: &ModuleConfig) -> Result<(), MergeConfigError> {
+impl<CVT: Clone> Merge for ModuleConfig<CVT> {
+    fn merge(&mut self, prev: &ModuleConfig<CVT>) -> Result<(), MergeConfigError> {
         if self.enable.is_none() {
             self.enable = prev.enable.clone();
         }
@@ -176,166 +108,141 @@ impl Merge for ModuleConfig {
     }
 }
 
-struct BrutalSessionHandler;
-
-impl StreamSessionHandler for BrutalSessionHandler {
-    const PHASE: StreamPhase = StreamPhase::PostAccept;
-    type Output = Status;
-
-    fn handler(session: &mut Session) -> Self::Output {
-        let brutal_param = Module::server_conf_mut(session)
-            .expect("module config is none")
-            .get_brutal_param(session);
-        let enable = brutal_param.is_some();
-        let connection = unsafe {
-            session
-                .connection()
+pub fn brutal_handler<T: NginxHandlerCtxTrait>(
+    ctx: &mut T,
+    brutal_param: Option<BrutalParams>,
+) -> Status {
+    let enable = brutal_param.is_some();
+    let connection = ctx.connection();
+    let should_apply_brutal = connection.type_ as u32 == __socket_type_SOCK_STREAM
+        && unsafe {
+            connection
+                .listening
                 .as_ref()
-                .expect("connection always not null")
+                .and_then(|listening| listening.sockaddr.as_ref())
+                .is_some_and(|sockaddr| sockaddr.sa_family as u32 != AF_UNIX)
         };
-        let should_apply_brutal = connection.type_ as u32 == __socket_type_SOCK_STREAM
-            && unsafe {
-                connection
-                    .listening
-                    .as_ref()
-                    .and_then(|listening| listening.sockaddr.as_ref())
-                    .is_some_and(|sockaddr| sockaddr.sa_family as u32 != AF_UNIX)
-            };
 
-        if let Some(brutal_param) = brutal_param
-            && should_apply_brutal
-        {
-            let fd = connection.fd;
+    if let Some(brutal_param) = brutal_param
+        && should_apply_brutal
+    {
+        let fd = connection.fd;
 
-            let log = session.log();
-            ngx_log_debug!(mask: DebugMask::Stream, log, "brutal module enabled: {enable}");
+        let log = ctx.log();
+        ngx_log_debug!(mask: <T as NginxHandlerCtxTrait>::DEBUG_MASK, log, "brutal module enabled: {enable}");
 
-            let algo = b"brutal\0";
+        let algo = b"brutal\0";
 
-            let set_sock_opt_ret = unsafe {
-                setsockopt(
-                    fd,
-                    IPPROTO_TCP as i32,
-                    TCP_CONGESTION as i32,
-                    algo.as_ptr().cast(),
-                    algo.len() as u32,
-                )
-            };
-            if set_sock_opt_ret != 0 {
-                ngx_log_error!(
-                    NGX_LOG_ERR,
-                    log,
-                    "tcp brutal TCP_CONGESTION 1 error: {set_sock_opt_ret}"
-                );
-                return Status::NGX_DECLINED;
-            }
-
-            const TCP_BRUTAL_PARAMS: u32 = 23301;
-            let set_sock_opt_ret = unsafe {
-                setsockopt(
-                    fd,
-                    IPPROTO_TCP as i32,
-                    TCP_BRUTAL_PARAMS as i32,
-                    (&brutal_param as *const BrutalParams).cast(),
-                    std::mem::size_of::<BrutalParams>() as u32,
-                )
-            };
-            if set_sock_opt_ret != 0 {
-                ngx_log_error!(
-                    NGX_LOG_ERR,
-                    log,
-                    "tcp brutal TCP_CONGESTION 2 error: {set_sock_opt_ret}"
-                );
-                return Status::NGX_DECLINED;
-            }
-            Status::NGX_OK
-        } else {
-            Status::NGX_DECLINED
+        let set_sock_opt_ret = unsafe {
+            setsockopt(
+                fd,
+                IPPROTO_TCP as i32,
+                TCP_CONGESTION as i32,
+                algo.as_ptr().cast(),
+                algo.len() as u32,
+            )
+        };
+        if set_sock_opt_ret != 0 {
+            ngx_log_error!(
+                NGX_LOG_ERR,
+                log,
+                "tcp brutal TCP_CONGESTION 1 error: {set_sock_opt_ret}"
+            );
+            return Status::NGX_DECLINED;
         }
+
+        const TCP_BRUTAL_PARAMS: u32 = 23301;
+        let set_sock_opt_ret = unsafe {
+            setsockopt(
+                fd,
+                IPPROTO_TCP as i32,
+                TCP_BRUTAL_PARAMS as i32,
+                (&brutal_param as *const BrutalParams).cast(),
+                std::mem::size_of::<BrutalParams>() as u32,
+            )
+        };
+        if set_sock_opt_ret != 0 {
+            ngx_log_error!(
+                NGX_LOG_ERR,
+                log,
+                "tcp brutal TCP_CONGESTION 2 error: {set_sock_opt_ret}"
+            );
+            return Status::NGX_DECLINED;
+        }
+        Status::NGX_OK
+    } else {
+        Status::NGX_DECLINED
     }
 }
 
-fn extract_nginx_stream_value<T: FromStr>(
+pub fn extract_nginx_value<T: FromStr, CVT: ComplexValueTrait>(
     mut cf: NonNull<ngx_conf_t>,
-    cmd: *mut ngx_command_t,
-) -> Option<NginxStreamValue<T>> {
-    unsafe {
-        let args: &mut [ngx_str_t] = (*cf.as_ref().args).as_slice_mut();
-        let cmd_name = (*cmd).name;
-        let cmd_name = if let Ok(cmd_name) = cmd_name.to_str() {
-            cmd_name
-        } else {
+    cmd: NonNull<ngx_command_t>,
+) -> Option<NginxValue<T, CVT>> {
+    let args: &mut [ngx_str_t] = unsafe { (*cf.as_ref().args).as_slice_mut() };
+    let cf = unsafe { cf.as_mut() };
+    let cmd_name = unsafe { cmd.as_ref() }.name;
+    let cmd_name = if let Ok(cmd_name) = cmd_name.to_str() {
+        cmd_name
+    } else {
+        ngx_conf_log_error!(NGX_LOG_EMERG, cf, "cmd name {} not utf-8 encoded", cmd_name);
+        return None;
+    };
+
+    let pool = unsafe { Pool::from_ngx_pool(cf.pool) };
+    let layout = Layout::new::<CVT>();
+    let cv = pool.allocate(unsafe { std::mem::zeroed::<CVT>() });
+    let mut cv = match NonNull::new(cv) {
+        Some(cv) => cv,
+        None => {
             ngx_conf_log_error!(
                 NGX_LOG_EMERG,
-                cf.as_mut(),
-                "cmd name {} not utf-8 encoded",
-                cmd_name
-            );
-            return None;
-        };
-
-        let pool = Pool::from_ngx_pool(cf.as_ref().pool);
-        let layout = Layout::new::<ngx_stream_complex_value_t>();
-        let cv = pool.allocate(std::mem::zeroed::<ngx_stream_complex_value_t>());
-        let mut cv = match NonNull::new(cv) {
-            Some(cv) => cv,
-            None => {
-                ngx_conf_log_error!(
-                    NGX_LOG_EMERG,
-                    cf.as_mut(),
-                    "failed to allocate ngx_stream_complex_value_t for `{cmd_name}`"
-                );
-                return None;
-            }
-        };
-
-        let mut ccv: ngx_stream_compile_complex_value_t = std::mem::zeroed();
-        ccv.cf = cf.as_mut();
-        ccv.value = &mut args[1] as _;
-        ccv.complex_value = cv.as_mut();
-
-        if ngx_stream_compile_complex_value(&mut ccv as _) != NGX_OK as ngx_int_t {
-            ngx_conf_log_error!(
-                NGX_LOG_EMERG,
-                cf.as_mut(),
-                "`{cmd_name}` argument ngx_stream_compile_complex_value failed."
+                cf,
+                "failed to allocate ngx complex_value_t for `{cmd_name}`"
             );
             return None;
         }
+    };
 
-        let val = if cv.as_ref().lengths.is_null() {
+    let mut ccv: CVT::NginxCompileComplexValueType = unsafe { std::mem::zeroed() };
+    CVT::init_compile_complex_value(&mut ccv, cf, &mut args[1] as _, unsafe { cv.as_mut() }
+        as *mut _);
+
+    if let Err(err) = CVT::compile(&mut ccv) {
+        ngx_conf_log_error!(
+            NGX_LOG_EMERG,
+            cf,
+            "`{cmd_name}` argument compile failed. err: {err}"
+        );
+        return None;
+    }
+
+    match CVT::static_value::<T>(unsafe { cv.as_ref() }) {
+        Ok(Some(static_value)) => {
             // 静态值
-            pool.deallocate(cv.cast::<u8>(), layout);
-            let val = match args[1].to_str() {
-                Ok(s) => s,
-                Err(_) => {
-                    ngx_conf_log_error!(
-                        NGX_LOG_EMERG,
-                        cf.as_mut(),
-                        "`{cmd_name}` argument is not utf-8 encoded"
-                    );
-                    return None;
-                }
-            };
-
-            let n = match val.parse::<T>() {
-                Ok(n) => n,
-                _ => {
-                    ngx_conf_log_error!(NGX_LOG_EMERG, cf.as_mut(), "invalid {cmd_name}",);
-                    return None;
-                }
-            };
-            NginxStreamValue::Static(n)
-        } else {
-            let conf_file = cf.as_ref().conf_file;
+            unsafe {
+                pool.deallocate(cv.cast::<u8>(), layout);
+            }
+            Some(NginxValue::Static(static_value))
+        }
+        Err(err) => {
+            // 静态值解析失败
+            unsafe {
+                pool.deallocate(cv.cast::<u8>(), layout);
+            }
+            ngx_conf_log_error!(NGX_LOG_EMERG, cf, "invalid {cmd_name}, err: {err}",);
+            None
+        }
+        Ok(None) => {
+            let conf_file = cf.conf_file;
             let (file_name, line) = if let Some(conf_file) = NonNull::new(conf_file) {
-                let conf_file = conf_file.as_ref();
+                let conf_file = unsafe { conf_file.as_ref() };
                 let file_name = if let Ok(file_name) = conf_file.file.name.to_str() {
                     file_name.to_string()
                 } else {
                     ngx_conf_log_error!(
                         NGX_LOG_EMERG,
-                        cf.as_mut(),
+                        cf,
                         "invalid config file name {}",
                         conf_file.file.name
                     );
@@ -343,63 +250,17 @@ fn extract_nginx_stream_value<T: FromStr>(
                 };
                 (file_name, conf_file.line as u32)
             } else {
-                ngx_conf_log_error!(NGX_LOG_EMERG, cf.as_mut(), "conf_file name is null",);
+                ngx_conf_log_error!(NGX_LOG_EMERG, cf, "conf_file name is null",);
                 return None;
             };
             // complex value，里面有变量
-            NginxStreamValue::Complex(NginxStreamComplexValue {
+            let val = NginxValue::Complex(NginxComplexValue {
                 cv,
                 cmd_name: cmd_name.to_string(),
                 file_name,
                 line,
-            })
-        };
-        Some(val)
-    }
-}
-
-extern "C" fn ngx_stream_brutal_set(
-    cf: *mut ngx_conf_t,
-    cmd: *mut ngx_command_t,
-    conf: *mut c_void,
-) -> *mut c_char {
-    let conf = unsafe { &mut *(conf as *mut ModuleConfig) };
-    if let Some(v) =
-        extract_nginx_stream_value::<NginxBool>(NonNull::new(cf).expect("cf not null"), cmd)
-    {
-        conf.enable = Some(v);
-        NGX_CONF_OK
-    } else {
-        NGX_CONF_ERROR
-    }
-}
-
-extern "C" fn ngx_stream_brutal_rate_commands_set(
-    cf: *mut ngx_conf_t,
-    cmd: *mut ngx_command_t,
-    conf: *mut c_void,
-) -> *mut c_char {
-    let conf = unsafe { &mut *(conf as *mut ModuleConfig) };
-    if let Some(v) = extract_nginx_stream_value::<u64>(NonNull::new(cf).expect("cf not null"), cmd)
-    {
-        conf.rate = Some(v);
-        NGX_CONF_OK
-    } else {
-        NGX_CONF_ERROR
-    }
-}
-
-extern "C" fn ngx_stream_brutal_cwnd_gain_commands_set(
-    cf: *mut ngx_conf_t,
-    cmd: *mut ngx_command_t,
-    conf: *mut c_void,
-) -> *mut c_char {
-    let conf = unsafe { &mut *(conf as *mut ModuleConfig) };
-    if let Some(v) = extract_nginx_stream_value::<u32>(NonNull::new(cf).expect("cf not null"), cmd)
-    {
-        conf.cwnd_gain = Some(v);
-        NGX_CONF_OK
-    } else {
-        NGX_CONF_ERROR
+            });
+            Some(val)
+        }
     }
 }
--- src/value.rs
+++ src/value.rs
@@ -3,64 +3,198 @@ use std::{
     str::{FromStr, ParseBoolError},
 };
 
-use nginx_sys::ngx_stream_complex_value_t;
+use nginx_sys::{
+    NGX_OK, ngx_conf_t, ngx_connection_t, ngx_http_compile_complex_value,
+    ngx_http_compile_complex_value_t, ngx_http_complex_value_t, ngx_int_t, ngx_log_t, ngx_str_t,
+    ngx_stream_compile_complex_value, ngx_stream_compile_complex_value_t,
+    ngx_stream_complex_value_t,
+};
+use ngx::{core::NgxStr, http::Request, log::DebugMask};
 
 use crate::stream::Session;
 
 #[derive(Debug, Clone)]
-pub enum NginxStreamValue<T> {
-    Complex(NginxStreamComplexValue),
+pub enum NginxValue<T, CVT> {
+    Complex(NginxComplexValue<CVT>),
     Static(T),
 }
 
 #[derive(Debug, Clone)]
-pub struct NginxStreamComplexValue {
-    pub cv: NonNull<ngx_stream_complex_value_t>,
+pub struct NginxComplexValue<CVT> {
+    pub cv: NonNull<CVT>,
     pub cmd_name: String,
     pub file_name: String,
     pub line: u32,
 }
 
-impl NginxStreamComplexValue {
-    pub fn extract<T: Clone + FromStr>(
+pub trait ComplexValueTrait {
+    type NginxCompileComplexValueType;
+    fn init_compile_complex_value(
+        compile_complex_value: &mut Self::NginxCompileComplexValueType,
+        cf: *mut ngx_conf_t,
+        value: *mut ngx_str_t,
+        complex_value: *mut Self,
+    );
+    fn compile(
+        compile_complex_value: &mut Self::NginxCompileComplexValueType,
+    ) -> Result<(), String>;
+    fn static_value<T: FromStr>(&self) -> Result<Option<T>, String>;
+}
+impl ComplexValueTrait for ngx_stream_complex_value_t {
+    type NginxCompileComplexValueType = ngx_stream_compile_complex_value_t;
+    fn init_compile_complex_value(
+        compile_complex_value: &mut Self::NginxCompileComplexValueType,
+        cf: *mut ngx_conf_t,
+        value: *mut ngx_str_t,
+        complex_value: *mut Self,
+    ) {
+        compile_complex_value.cf = cf;
+        compile_complex_value.value = value;
+        compile_complex_value.complex_value = complex_value;
+    }
+
+    fn compile(
+        compile_complex_value: &mut Self::NginxCompileComplexValueType,
+    ) -> Result<(), String> {
+        if unsafe { ngx_stream_compile_complex_value(compile_complex_value) } != NGX_OK as ngx_int_t
+        {
+            return Err("ngx_stream_compile_complex_value failed.".to_string());
+        }
+        Ok(())
+    }
+
+    fn static_value<T: FromStr>(&self) -> Result<Option<T>, String> {
+        if self.lengths.is_null() {
+            Ok(Some(parse_nginx_str(unsafe {
+                NgxStr::from_ngx_str(self.value)
+            })?))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl ComplexValueTrait for ngx_http_complex_value_t {
+    type NginxCompileComplexValueType = ngx_http_compile_complex_value_t;
+    fn init_compile_complex_value(
+        compile_complex_value: &mut Self::NginxCompileComplexValueType,
+        cf: *mut ngx_conf_t,
+        value: *mut ngx_str_t,
+        complex_value: *mut Self,
+    ) {
+        compile_complex_value.cf = cf;
+        compile_complex_value.value = value;
+        compile_complex_value.complex_value = complex_value;
+    }
+    fn compile(
+        compile_complex_value: &mut Self::NginxCompileComplexValueType,
+    ) -> Result<(), String> {
+        if unsafe { ngx_http_compile_complex_value(compile_complex_value) } != NGX_OK as ngx_int_t {
+            return Err("ngx_http_compile_complex_value failed.".to_string());
+        }
+        Ok(())
+    }
+
+    fn static_value<T: FromStr>(&self) -> Result<Option<T>, String> {
+        if self.lengths.is_null() {
+            Ok(Some(parse_nginx_str(unsafe {
+                NgxStr::from_ngx_str(self.value)
+            })?))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+fn parse_nginx_str<T: FromStr>(s: &NgxStr) -> Result<T, String> {
+    let s = s
+        .to_str()
+        .map_err(|_| "not utf-8 encoded nginx str".to_string())?;
+    s.parse::<T>().map_err(|_| format!("cannot parse `{s}`"))
+}
+
+pub trait NginxHandlerCtxTrait {
+    type NginxComplexValueType;
+    const DEBUG_MASK: DebugMask;
+    fn get_complex_value<T: FromStr>(
         &mut self,
-        session: Option<&mut Session>,
+        cv: &mut NginxComplexValue<Self::NginxComplexValueType>,
     ) -> Result<T, String> {
-        if let Some(session) = session {
-            if let Some(vs) = session.get_complex_value(unsafe { self.cv.as_mut() }) {
-                if let Ok(vs) = vs.to_str() {
-                    vs.parse::<T>().map_err(|_| {
-                        format!(
-                            "Cannot parse `{}`: `{vs}` at file: {}, line: {}",
-                            self.cmd_name, self.file_name, self.line
-                        )
-                    })
-                } else {
-                    Err(format!(
-                        "Cannot parse `{}` at file: {}, line: {} with not utf-8 encoded nginx str {}",
-                        self.cmd_name, self.file_name, self.line, vs
-                    ))
-                }
-            } else {
-                Err(format!(
-                    "Cannot get_complex_value for `{}` at file: {}, line: {}",
-                    self.cmd_name, self.file_name, self.line,
-                ))
-            }
+        if let Some(vs) = self.get_nginx_complex_value(unsafe { cv.cv.as_mut() }) {
+            parse_nginx_str(vs).map_err(|err| {
+                format!(
+                    "Cannot parse `{}` at file: {}, line: {}, err: {err}",
+                    cv.cmd_name, cv.file_name, cv.line
+                )
+            })
         } else {
             Err(format!(
-                "Cannot parse `{}` at file: {}, line: {} with None session",
+                "Cannot get_complex_value for `{}` at file: {}, line: {}",
+                cv.cmd_name, cv.file_name, cv.line,
+            ))
+        }
+    }
+    fn get_nginx_complex_value(&mut self, cv: &mut Self::NginxComplexValueType) -> Option<&NgxStr>;
+    fn log(&self) -> *mut ngx_log_t;
+    fn connection(&self) -> &ngx_connection_t;
+}
+
+impl NginxHandlerCtxTrait for Session {
+    type NginxComplexValueType = ngx_stream_complex_value_t;
+    const DEBUG_MASK: DebugMask = DebugMask::Stream;
+
+    fn get_nginx_complex_value(&mut self, cv: &mut Self::NginxComplexValueType) -> Option<&NgxStr> {
+        self.get_complex_value(cv)
+    }
+    fn log(&self) -> *mut ngx_log_t {
+        self.log()
+    }
+    fn connection(&self) -> &ngx_connection_t {
+        let conn = self.connection();
+        unsafe { conn.as_mut() }.expect("connection always not null")
+    }
+}
+
+impl NginxHandlerCtxTrait for Request {
+    type NginxComplexValueType = ngx_http_complex_value_t;
+    const DEBUG_MASK: DebugMask = DebugMask::Http;
+
+    fn get_nginx_complex_value(&mut self, cv: &mut Self::NginxComplexValueType) -> Option<&NgxStr> {
+        Request::get_complex_value(self, cv)
+    }
+    fn log(&self) -> *mut ngx_log_t {
+        self.log()
+    }
+    fn connection(&self) -> &ngx_connection_t {
+        let conn = self.connection();
+        unsafe { conn.as_mut() }.expect("connection always not null")
+    }
+}
+
+impl<CVT> NginxComplexValue<CVT> {
+    pub fn extract<T: Clone + FromStr, CTX: NginxHandlerCtxTrait<NginxComplexValueType = CVT>>(
+        &mut self,
+        ctx: Option<&mut CTX>,
+    ) -> Result<T, String> {
+        if let Some(ctx) = ctx {
+            ctx.get_complex_value::<T>(self)
+        } else {
+            Err(format!(
+                "Cannot parse `{}` at file: {}, line: {} with None ctx",
                 self.cmd_name, self.file_name, self.line
             ))
         }
     }
 }
 
-impl<T: Clone + FromStr> NginxStreamValue<T> {
-    pub fn extract(&mut self, session: Option<&mut Session>) -> Result<T, String> {
+impl<T: Clone + FromStr, CVT> NginxValue<T, CVT> {
+    pub fn extract<CTX: NginxHandlerCtxTrait<NginxComplexValueType = CVT>>(
+        &mut self,
+        ctx: Option<&mut CTX>,
+    ) -> Result<T, String> {
         match self {
-            NginxStreamValue::Complex(value, ..) => value.extract(session),
-            NginxStreamValue::Static(v) => Ok(v.clone()),
+            NginxValue::Complex(value, ..) => value.extract(ctx),
+            NginxValue::Static(v) => Ok(v.clone()),
         }
     }
 }
